1487c3ed71756664920bac90a2cc39edb451b6b1
Petar Jakopec <petar.jakopec0@gmail.com>
2024-04-02 11:54:31 +0200

    NGSTACK-756 add bundle class

---
+++ bundle/NetgenIbexaImportExportBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\IbexaImportExportBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NetgenIbexaImportExportBundle extends Bundle
+{
+    public function build(ContainerBuilder $container): void {}
+}
