49d2a48350321db16478b56ad12bc7ffec0bb828
Petar Jakopec <petar.jakopec0@gmail.com>
2024-08-23 12:10:41 +0200

    NGSTACK-756 remove dump

--- lib/Core/FieldHandler/EzBinaryFile.php
+++ lib/Core/FieldHandler/EzBinaryFile.php
@@ -10,7 +10,6 @@ use Kaliop\eZMigrationBundle\API\FieldValueConverterInterface;
 use Kaliop\eZMigrationBundle\Core\FieldHandler\FileFieldHandler;
 
 use function basename;
-use function dump;
 use function filesize;
 use function is_file;
 use function is_string;
@@ -73,10 +72,6 @@ final class EzBinaryFile extends FileFieldHandler implements FieldValueConverter
 
     public function fieldValueToHash($fieldValue, array $context = []): ?array
     {
-        dump($this->ioRootDir);
-        dump(realpath($this->ioRootDir));
-
-        exit;
         if ($fieldValue->uri === null) {
             return null;
         }
