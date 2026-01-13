26ef8dc85018fe3a67a22d8bb06df54c1cc5a2de
Petar Jakopec <petar.jakopec0@gmail.com>
2026-01-13 06:55:55 +0100

    NGSTACK-756 harden download controller

--- bundle/Controller/Export/Download.php
+++ bundle/Controller/Export/Download.php
@@ -9,6 +9,12 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+use function basename;
+use function is_string;
+use function realpath;
+use function str_starts_with;
 
 final class Download extends AbstractController
 {
@@ -20,8 +26,20 @@ final class Download extends AbstractController
     {
         $this->denyAccessUnlessGranted('ibexa:import_export:access');
 
+        if (!is_string($file) || $file === '' || basename($file) !== $file) {
+            throw new NotFoundHttpException();
+        }
+
         $projectRoot = $this->container->getParameter('kernel.project_dir');
+
+        $migrationsDir = realpath($projectRoot . '/' . $this->migrationsPath);
         $filePath = $projectRoot . '/' . $this->migrationsPath . '/' . $file;
+        $realFilePath = realpath($filePath);
+
+        if ($migrationsDir === false || $realFilePath === false || !str_starts_with($realFilePath, $migrationsDir)) {
+            throw new NotFoundHttpException();
+        }
+
         $response = new BinaryFileResponse($filePath);
         $response->headers->set('Content-Type', 'application/octet-stream');
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $file);
